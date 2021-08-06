5b6cd12643f3428a06fa93d7b7efb41b5a5d8e16
pgm-thabisadingani <71275598+pgm-thabisadingani@users.noreply.github.com>
2021-08-07 00:10:07 +0200

    avatar fixed

--- components/Avatar.js
+++ components/Avatar.js
@@ -5,6 +5,8 @@ export default function Avatar({ url, className }){
     <Image 
       loading="lazy"
       src={url}
+      height={50}
+      width={50}
       alt="profile pic"
       className={`rounded-full h-12 cursor-pointer transition duration-150 transform hover:scale-110 ${className}`}
     />
